5a1e7277893960a4ec4d45da314566f6c4ab2f32
JoseFloresOrdo <109826426+JoseFloresOrdo@users.noreply.github.com>
2022-11-08 21:04:42 -0600

    Add files via upload

---
+++ modoTelefono.java
@@ -0,0 +1,10 @@
+
+public interface modoTelefono {
+    public void conectado();
+    public String mostrarConstactos();
+    public String llamarContactos(int desicion);
+    public String finalizarLlamadaEspera();
+    public String cambiarLlamadaEspera();
+
+    
+}
